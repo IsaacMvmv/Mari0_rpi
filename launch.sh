33694160cf6a081a1d945c4622b3332cb78b3976
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2021-05-27 16:53:46 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-LD_LIBRARY_PATH=love_armhf:$LD_LIBRARY_PATH ./love_armhf/love mari0.love
+LD_LIBRARY_PATH=love_armhf:$LD_LIBRARY_PATH $HOME/Mari0_rpi/love_armhf/love $HOME/Mari0_rpi/mari0.love
